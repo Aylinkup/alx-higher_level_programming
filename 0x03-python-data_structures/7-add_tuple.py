5c52f84a9ab3a815c0ae861c2a1cb9c4f336db32
Aylinkup <www.fxace@gmail.com>
2022-12-06 13:12:12 -0800

    Python program - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
-        tuple_a += (0, 0)
-    elif len(tuple_a) == 1:
-        tuple_a += (0,)
-    elif len(tuple_a) > 2:
-        tuple_a = tuple_a[:2]
-
-    if len(tuple_b) == 0:
-        tuple_b += (0, 0)
-    elif len(tuple_b) == 1:
-        tuple_b += (0,)
-    elif len(tuple_b) > 2:
-        tuple_b = tuple_b[:2]
-    res = tuple(sum(item) for item in zip(tuple_a, tuple_b))
-    return res 
+    a1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
+    b1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    b2 = tuple_b[1] if len(tuple_b) > 1 else 0
+    return (a1 + b1, a2 + b2)
